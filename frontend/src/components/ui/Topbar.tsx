28da7631d38feddd45da541f8787ed90a45c2035
Hien Long Nguyen <lnguyen106@fordham.edu>
2026-03-20 22:00:35 -0400

    Fix frontend being tracked as submodule

---
+++ frontend/src/components/ui/Topbar.tsx
@@ -0,0 +1,36 @@
+import { LayoutDashboardIcon } from "lucide-react"
+import { Link } from "react-router-dom"
+import { Show } from "@clerk/react"
+import SignInOAuthButtons from "./SignInOAuthButtons"
+import { SignOutButton } from "@clerk/react";
+
+const Topbar = () => {
+    const isAdmin = false
+    return (
+        <div className="flex items-center justify-between p-4 sticky top-0 bg-zinc-900/75
+        backdrop-blur-md z-10">
+            <div className="flex gap-2 items-center">
+                Spotify
+            </div>
+            <div className="flex items-center gap-4">
+                {isAdmin && (
+                    <Link to={"/admin"}>
+                        <LayoutDashboardIcon className="size-4 mr-2"/>
+                            Admin Dashboard
+                    </Link>
+                )}
+
+                <Show when="signed-in">
+                    <SignOutButton />
+                </Show>
+
+                <Show when="signed-out">     
+                    <SignInOAuthButtons />
+                </Show>
+            </div>
+        </div>
+            
+    )
+}
+
+export default Topbar
---
+++ frontend/src/main.tsx
@@ -0,0 +1,25 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { ClerkProvider } from '@clerk/react'
+import { BrowserRouter } from 'react-router-dom'
+import './index.css'
+import App from './App.tsx'
+import AuthProvider from './providers/AuthProvider.tsx'
+
+const publishableKey = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY
+
+if (!publishableKey) {
+  throw new Error("Missing VITE_CLERK_PUBLISHABLE_KEY environment variable")
+}
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <ClerkProvider publishableKey={publishableKey} afterSignOutUrl='/'>
+      <AuthProvider>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </AuthProvider>
+    </ClerkProvider>
+  </StrictMode>,
+)
