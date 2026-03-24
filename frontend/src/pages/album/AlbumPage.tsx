a1f7cb4ff9dc99e653fa65b7f962019aa729d61f
Hien Long Nguyen <lnguyen106@fordham.edu>
2026-03-24 18:53:04 -0400

    AlbumPage general layout done

--- frontend/src/pages/album/AlbumPage.tsx
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -2,7 +2,14 @@ import { useParams } from "react-router-dom"
 import { useEffect } from "react"
 import { useMusicStore } from "@/stores/useMusicStore"
 import { ScrollArea } from "@/components/ui/scroll-area";
+import { Button } from "@/components/ui/button";
+import { Clock, Play } from "lucide-react";
 
+const formatDuration = (duration: number) => {
+    const minutes = Math.floor(duration / 60);
+    const seconds = duration % 60;
+    return `${minutes}:${seconds < 10 ? `0${seconds}` : seconds}`
+}
 const AlbumPage = () => {
 
    const { albumId } = useParams();
@@ -43,8 +50,62 @@ const AlbumPage = () => {
                                 <span>• {currentAlbum.songs.length} songs</span>
                                 <span>• {currentAlbum.releaseYear}</span>
                             </div>
-
                         </div>
+                    </div>
+
+                    {/* play button */}
+                    <div className='px-6 pb-4 flex items-center gap-6'>
+                        <Button
+                        size='icon'
+                        className='w-14 h-14 rounded-full bg-blue-600 hover:bg-blue-500 hover:scale-105 transition-all'
+                        >
+                            <Play className='h-7 w-7 text-black' />
+                        </Button>
+                    </div>
+
+                    {/* table section */}
+                    <div className="bg-black/20 backdrop-blur-sm">
+                    {/* table header */}
+                    <div className='grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-10 py-2 text-sm text-zinc-400
+                    border-b border-white/5'>
+                        <div>#</div>
+                        <div>Title</div>
+                        <div>Release Date</div>
+                        <div>
+                            <Clock className='h-4 w-4' />
+                        </div>
+                    </div>
+
+                    {/* songs list */}
+                    <div className='px-6'>
+                        <div className="space-y-2 py-4">
+                            {currentAlbum?.songs.map((song,index) => (
+                                <div key={song._id}
+                                className={
+                                    'grid grid-cols-[16px_4fr_2fr_1fr] gap-4 px-4 py-2 text-sm text-zinc-400hover:bg-white/5 rounded-md group cursor-pointer'}>
+                                     <div className='flex items-center justify-center'>
+                                        <span className="group-hover:hidden">{index + 1}</span>
+                                        <Play className='h-4 w-4 hidden group-hover:block' />
+                                     </div>
+
+                                     <div className="flex items-center gap-3">
+                                        <img src={song.imageUrl} alt={song.title} 
+                                        className='size-10'/>
+
+                                        <div>
+                                            <div className={'font-medium text-white'}>{song.title}</div>
+                                            <div>{song.artist}</div>
+                                        </div>
+                                     </div>
+                                    <div className='flex items-center'>{song.createdAt.split("T")[0]}</div>
+                                    <div className='flex items-center'>{formatDuration(song.duration)}</div>
+
+                                </div>
+                            ))}   
+
+                        </div>   
+
+                    </div>
                     </div>
                 </div>
             </div>
