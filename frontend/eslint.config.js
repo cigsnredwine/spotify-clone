de6aec59813296259de7800f71956957dc412da8
Hien Long Nguyen <lnguyen106@fordham.edu>
2026-03-23 23:07:23 -0400

    readded backend scripts

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,6 +19,10 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-unused-vars': ['error', { varsIgnorePattern: '^React$' }],
+    },
   },
   {
     files: ['src/components/ui/**/*.{ts,tsx}'],
---
+++ frontend/src/pages/album/AlbumPage.tsx
@@ -0,0 +1,56 @@
+import { useParams } from "react-router-dom"
+import { useEffect } from "react"
+import { useMusicStore } from "@/stores/useMusicStore"
+import { ScrollArea } from "@/components/ui/scroll-area";
+
+const AlbumPage = () => {
+
+   const { albumId } = useParams();
+   const { fetchAlbumById, currentAlbum, isLoading } = useMusicStore();
+
+   useEffect(() => {
+      if(albumId) fetchAlbumById(albumId);
+   }, [fetchAlbumById, albumId])
+
+   if(isLoading) return null
+   
+  return (
+    <div className='h-full'>
+        <ScrollArea className='h-full'>
+            {/* Main content */}
+            <div className='relative min-h-full'>
+                {/* bg gradient */}
+                <div className='absolute inset-0 bg-gradient-to-b from-[#3793a6]/80 via-zinc-900/80
+                to-zinc-900 pointer-events-home'
+                aria-hidden='true'
+                />
+
+                {/* Content */}
+                <div className='relative z-10'>
+                    <div className='flex p-6 gap-6 pb-8'>
+                        <img src={currentAlbum?.imageUrl} alt={currentAlbum?.title}
+                        className='w-[240px] h-[240px] shawdow-x1 rounded'
+                        />
+                        <div className="flex flex-col justify-end">
+                            <p className='text-sm font-medium'>
+                                Album
+                            </p>
+                            <h1 className='text-7xl font-bold my-4'>
+                                {currentAlbum?.title}
+                            </h1>
+                            <div className="flex items-center gap-2 text-sm text-zinc-100">   
+                                <span className="font-medium text-white">{currentAlbum?.artist}</span>
+                                <span>• {currentAlbum?.songs.length} songs</span>
+                                <span>• {currentAlbum?.songs.length} songs</span>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </ScrollArea>
+    </div>
+  );
+}
+
+export default AlbumPage
